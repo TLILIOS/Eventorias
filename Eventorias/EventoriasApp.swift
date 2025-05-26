9a369c153cbde9ce842b11278ecab10fe5913def
TLiLi Hamdi <tlilios@yahoo.com>
2025-05-26 19:16:12 +0200

    First commit

---
+++ Eventorias/EventoriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventoriasApp.swift
+//  Eventorias
+//
+//  Created by TLiLi Hamdi on 26/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct EventoriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
