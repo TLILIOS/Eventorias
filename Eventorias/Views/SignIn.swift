07659a1f3f6604f92b6f6839b9596c4b50664202
TLiLi Hamdi <tlilios@yahoo.com>
2025-05-27 03:37:13 +0200

    Installing assets and configuring colors

---
+++ Eventorias/Views/SignIn.swift
@@ -0,0 +1,18 @@
+//
+//  Sign-in.swift
+//  Eventorias
+//
+//  Created by TLiLi Hamdi on 27/05/2025.
+//
+
+import SwiftUI
+
+struct Sign_in: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sign_in()
+}
